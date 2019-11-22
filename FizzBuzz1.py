ab7566c1d6d05534f38457c6c57767b9fa4f4a16
Stephen Cole <stephenjcole@msn.com>
2019-11-22 12:35:16 -0800

    Added some whitespace

--- FizzBuzz1.py
+++ FizzBuzz1.py
@@ -3,14 +3,18 @@ for i in range(1, 101):
     # I chose the modulo of 3 *and* the modulo of 5
     # for readability.  (i mod 15) would probably have been 
     # simpler.  However, I opted to mirror the requirement.
+    
     if ( ( (i % 3) == 0 ) and ( (i % 5) == 0) ):
         print("FizzBuzz")
+        
     # Check for multiple of 3.
     elif( (i % 3) == 0 ):
         print("Fizz")
+        
     # Check for a multiple of 5.
     elif( (i % 5) == 0 ):
         print("Buzz")
+        
     # For everything else, print the number.
     else:
         print(i)
